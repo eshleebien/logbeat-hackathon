566c01830c0e494dfdce54119e5bc3a5633da695
Esh <eshlee.romero@gengo.com>
2017-07-28 12:30:14 +0800

    Updated logcontroller

--- controllers/LogController.py
+++ controllers/LogController.py
@@ -1,15 +1,13 @@
 import falcon
 import json
-import logging
 
-from util import sqs, parselogs
-from config import config
+from util import parselogs, slack
 
 
 class Resource:
     def on_post(self, req, resp):
 
-        text = req.get_param('text').split();
+        text = req.get_param('text').split()
 
         q = {
                 'project': text[0],
@@ -17,42 +15,24 @@ class Resource:
                 'to': text[2]
         }
 
-        print(q);
+        print(q)
 
         resp.body = 'hello world'
         # query logs here
         logs = parselogs.parse_log_file(q['project'])
 
-        d_message_body = {
-            #"text": log.text,
-            "text": "test",
-            "attachments": [
-                {
-                    "text": "Todo next",
-                    #"fallback": "You are unable to choose a game",
-                    #"callback_id": "wopr_game",
-                    #"color": "#3AA3E3",
-                    "attachment_type": "default",
-                    "actions": [
-                        {
-                            "name": "create",
-                            "text": "Create Ticket",
-                            "type": "button",
-                            "value": "1",
-                            "confirm": {
-                                "title": "Are you sure?",
-                                "text": "Wouldn't you prefer a good game of chess?",
-                                "ok_text": "Yes",
-                                "dismiss_text": "No"
-                            }
-                        },
-                    ]
-                }
-            ]
-        }
+        attachments = []
+        for log in logs:
+            attachments.append(slack.format_log_attachments(log))
 
-        #dummy lines, to prevent pylint from complaining that we don't use these
+        response = {
+                "text": "",
+                "attachments": attachments
+            }
+
+        # dummy lines, to prevent pylint from complaining that we don't use these
         d_message_body
         logs
 
         resp.status = falcon.HTTP_200
+        resp.body = (json.dumps(response))
