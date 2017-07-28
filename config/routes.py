5d2648fef3f50a5ca4ce450d3a8748ab2de4ee32
Esh <eshlee.romero@gengo.com>
2017-07-28 11:05:59 +0800

    Added routes, added controller for logs

--- config/routes.py
+++ config/routes.py
@@ -4,3 +4,4 @@ class Router:
     def __init__(self, app):
         app.add_route('/', Request.Resource())
         app.add_route('/callback', Callback.Resource())
+        app.add_route('/logs', LogController.Resource())
---
+++ controllers/LogController.py
@@ -0,0 +1,54 @@
+import falcon
+import json
+import logging
+
+from util import sqs
+from config import config
+
+
+class Resource:
+    def on_post(self, req, resp):
+
+        text = req.get_param('text').split();
+
+        q = {
+                'project': text[0],
+                'from': text[1],
+                'to': text[2]
+        }
+
+        print(q);
+
+        resp.body = 'hello world'
+        # query logs here
+        logs = {};
+
+        d_message_body = {
+            #"text": log.text,
+            "text": "test",
+            "attachments": [
+                {
+                    "text": "Todo next",
+                    #"fallback": "You are unable to choose a game",
+                    #"callback_id": "wopr_game",
+                    #"color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": "create",
+                            "text": "Create Ticket",
+                            "type": "button",
+                            "value": "1"
+                            "confirm": {
+                                "title": "Are you sure?",
+                                "text": "Wouldn't you prefer a good game of chess?",
+                                "ok_text": "Yes",
+                                "dismiss_text": "No"
+                            }
+                        },
+                    ]
+                }
+            ]
+        }
+
+        resp.status = falcon.HTTP_200
